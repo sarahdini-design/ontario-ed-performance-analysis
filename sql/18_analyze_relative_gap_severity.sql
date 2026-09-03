7ff3c5fff4db6ef51411402c46788e12f18cc9ff
Sarah <sarah.diinii@gmail.com>
2026-09-03 13:11:46 -0400

    Update 18_analyze_relative_gap_severity.sql

--- sql/18_analyze_relative_gap_severity.sql
+++ sql/18_analyze_relative_gap_severity.sql
@@ -1,4 +1,3 @@
--- Purpose:
 -- Compare the relative severity of persistent performance gaps across
 -- (gap severity as a percentage of peer benchmark)
 -- the two ED indicators by expressing each gap as a percentage of the
